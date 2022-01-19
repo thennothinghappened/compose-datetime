2557cb3fe0cece1f52de9404251164774adda4b0
Waqas Tahir <wakaztahir@gmail.com>
2022-01-19 11:47:34 +0000

    changed minSdk to 21

--- buildSrc/src/main/kotlin/ProjectConfig.kt
+++ buildSrc/src/main/kotlin/ProjectConfig.kt
@@ -1,10 +1,10 @@
 object ProjectConfig {
     val compileSdk = 31
-    val minSdk = 23
+    val minSdk = 21
     val targetSdk = 30
 
     object Info {
-        val group : String = "com.vanpra.dialogs"
+        val group : String = "com.vanpra"
         val version : String = "1.0.0"
     }
 }
